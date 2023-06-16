a97ea416b01748f32b0cec13a44802bf0a68de99
VijhnAustin <vijhn.austin@gmail.com>
2023-06-15 23:53:05 -0500

    Adding logo to video still

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import VideoThumb from '@/public/images/hero-image-01.jpg'
+import VideoThumb from '@/public/images/public/images/MarcelinEngineeringPrint.jpg'
 import ModalVideo from '@/components/modal-video'
 
 export default function Hero() {
@@ -28,10 +28,10 @@ export default function Hero() {
             <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Marcelin Engineering is a fully licensed and full service structural and civil engineering firm with 30 years of collective experience serving Southeastern Louisiana and Belize, Central America.</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Our Work</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">About us</a>
               </div>
             </div>
           </div>
