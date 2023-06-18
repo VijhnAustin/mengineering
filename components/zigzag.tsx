876db4d6d7948090b774301193405f3edede3eab
VijhnAustin <vijhn.austin@gmail.com>
2023-06-18 14:15:28 -0500

    Mass changes to links and placeholder text

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 
-import FeatImage01 from '@/public/images/features-03-image-01.png'
-import FeatImage02 from '@/public/images/features-03-image-02.png'
-import FeatImage03 from '@/public/images/features-03-image-03.png'
+import FeatImage01 from '@/public/images/mengi1.jpg'
+import FeatImage02 from '@/public/images/mengi2.jpg'
+import FeatImage03 from '@/public/images/mengi3.jpg'
 
 export default function Zigzag() {
   return (
@@ -13,7 +13,7 @@ export default function Zigzag() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
-            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
+            <h1 className="h2 mb-4">One engineering firm, unlimited solutions</h1>
             <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
           </div>
 
