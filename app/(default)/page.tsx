7309036d3bcc1317e870b2298fc6f787966b5e68
VijhnAustin <vijhn.austin@gmail.com>
2023-06-18 22:06:35 -0500

    metadata change

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Marcelin Engineering',
+  description: 'Marcelin Engineering',
 }
 
 import Hero from '@/components/hero'
